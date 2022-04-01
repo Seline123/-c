3c39fa51be65c6c829bcb48b748600210b6b54b8
Seline123 <90691118+Seline123@users.noreply.github.com>
2022-04-01 13:04:21 +0200

    les 2 opdr 2

---
+++ Arrays.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace myNum
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int[] myNum = { 10, 20, 30, 40, 50, 60, };
+            Console.WriteLine(myNum[0]);
+        }
+    }
+}
