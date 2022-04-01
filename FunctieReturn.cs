76b1cb1f5e9b2a6bec1d3f9cbe9a7e47be3ceb45
Seline123 <90691118+Seline123@users.noreply.github.com>
2022-04-01 12:13:36 +0200

    Les 1

---
+++ FunctieReturn.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Name : ");
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Heyy " + Name);
+        }
+    }
+}
