0aa1424a3a20fbaae02c581f674405de267107b3
Seline123 <90691118+Seline123@users.noreply.github.com>
2022-04-01 12:31:54 +0200

    les 2

---
+++ Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FirstName = "Emma ";
+            int Age = 21;
+            bool Name = false;
+
+        }
+    }
+}
+
+    
